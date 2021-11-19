b8023ccc9d6ed786306939a0444e856abe684176
Valdenir Silva Brito <59507605+ValdenirSBrito@users.noreply.github.com>
2021-11-18 22:14:33 -0400

    commit

--- services/tarefas.service.js
+++ services/tarefas.service.js
@@ -3,11 +3,29 @@ const Tarefa = require('./../models/tarefa');
 
 class tarefasService {
   // vai conectar com o banco de dados e retornar a lista de vagas
-  findAll = async () => await Tarefa.find();
+  findAll = async () => await TarefasModel.find();
 
-  // vai buscar um unico item no banco de acordo com o seu id
-  findById = async (id) => await Tarefa.findById(id);
+  // buscar uma musica por id
+  findById = async (id) => {
+    return await TarefasModel.findById(id)
+  };
+  // recebe um objeto e salva no banco de dados.
+  create = async (tarefa) => {
+    return await TarefasModel.create(tarefa)  
+  }
+
+  // recebe um id e um objeto para ser atualizado no banco.
+  edit = async (id, tarefa) => {
+    return await TarefasModel.updateOne({ _id: id}, tarefa)
+  }
+
+  // recebe umm id e exclui a musica do banco de acordo com esse id.
+  delete = async (id) => {
+    return await TarefasModel.deleteOne({ _id: id})
+  }
 
 }
+
+
 
 module.exports = tarefasService;
