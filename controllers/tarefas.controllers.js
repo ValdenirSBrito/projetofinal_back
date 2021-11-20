bbeb8d8938a78764c12e7f775272c803b96dcc01
Valdenir Silva Brito <59507605+ValdenirSBrito@users.noreply.github.com>
2021-11-20 19:48:01 -0400

    commit

--- controllers/tarefas.controllers.js
+++ controllers/tarefas.controllers.js
@@ -1,12 +1,10 @@
-// importo o servico
+
 const TarefasService = require('./../services/tarefas.service');
 
-// inicializo a classe do servico para poder acessar seus metodos
 const tarefasService = new TarefasService;
 
 class tarefasController {
   getTarefas = async (req, res) => {
-    // acesso a funcao do service para acessar a lista de vagas do banco
     const tarefas = await tarefasService.findAll();
     res.send(tarefas)
   }
@@ -16,12 +14,10 @@ class tarefasController {
     const tarefa = await tarefasService.findById(idParam);
     res.send(tarefa);
   }
-}
 
-// funcao que cadastra a tarefa recebida pelo front no banco de dados
+
+
 createTarefa = async (req, res) => {
-  // acesso o corpo da requisicao para pegar o objeto.
-  // objeto para ser cadastrado no banco.
   const tarefa = req.body;
   if(!req.body){
     return;
@@ -36,10 +32,8 @@ createTarefa = async (req, res) => {
   })
 }
 
-// atualiza uma tarefa de acordo com o id e objeto recebido para ser atualizado.
 editTarefa = async (req, res) => {
   const idParam = req.params.id;
-  // pegamos o objeto com os dados atualizado para atualizar no banco.
   const tarefaEdit = req.body;
   await tarefasService.edit(idParam, tarefaEdit)
   .then(() => {
@@ -50,7 +44,6 @@ editTarefa = async (req, res) => {
   })
 }
 
-// recebe um id via parametro e exclui uma tarefa de acordo com esse id
 deleteTarefa = async (req, res) => {
   const idParam = req.params.id;
   await tarefasService.delete(idParam)
@@ -60,6 +53,7 @@ deleteTarefa = async (req, res) => {
   .catch(err => {
     res.status(500).send({message: `Error: ${err}`});
   })
+}
 }
 
 
