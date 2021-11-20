b93a7adb1db60cc5717620fcb665bbdde2d0dc31
Valdenir Silva Brito <59507605+ValdenirSBrito@users.noreply.github.com>
2021-11-20 19:27:55 -0400

    commit

--- conn/conn.js
+++ conn/conn.js
@@ -1,9 +1,6 @@
-// importar o mongosse para poder acessar os seus metodos
+
 const mongoose = require('mongoose');
 
-// url de conexao = mongodb://servidor:porta/nomedobanco
-// useNewUrlParser = fala pro mongo usar o novo sistema de urls
-// useUnifiedTopology = mecanismo de monitoramento do banco de dados
 
 const Conn = () => {
   	mongoose.connect('mongodb://localhost:27017/tarefas', {
@@ -14,5 +11,4 @@ const Conn = () => {
     }).catch(err => console.log('erro de conexao com o banco', err));
 }
 
-// exportando a funcao Conn para poder ser usada em outros locais
 module.exports = Conn;
