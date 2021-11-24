01d3379bf94dd2bdacba4ad02a52a3c020697f89
Valdenir Silva Brito <59507605+ValdenirSBrito@users.noreply.github.com>
2021-11-23 20:15:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
-if(process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
 const express = require('express');
-const cors = require('cors');
-
-const Conn = require('./conn/conn');
-
+const cors  = require('cors');
 const TarefasRouter = require('./routes/tarefas.routes');
-const UserRouter = require('./routes/user.routes');
-
+const Conn = require('./conn/conn');
 const app = express();
+
 app.use(express.json());
+
 app.use(cors());
 
 app.use('/tarefas', TarefasRouter);
-app.use('/user', UserRouter);
+
+if(process.env.NODE_ENV !== 'production'){
+  require('dotenv').config()
+};
+const db_url= process.env.DB_URL;
+const db_user= process.env.DB_USER;
+const db_pass= process.env.DB_PASS;
+const db_data= process.env.DB_DATA;
+
+console.log(db_url, db_user, db_data, db_pass)
+Conn(db_url, db_user, db_pass ,db_data);
 
 
-const db_url = process.env.DB_URL;
-const db_user = process.env.db_user;
-const db_pass = process.env.DB_PASS;
-const db_data = process.env.DB_DATA;
-
-Conn(db_url, db_user, db_pass, db_data);
-
-const port = 3000;
-
+const port = 3001;
 app.listen(process.env.PORT || port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
-})
+  console.log(`servidor rodando na porta ${port}`)
+});
